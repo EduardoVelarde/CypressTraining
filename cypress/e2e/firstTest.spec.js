49ebc2d74b80764365793779e9d6c4843367fe70
EduardoVelarde <55457194+EduardoVelarde@users.noreply.github.com>
2023-12-15 16:48:32 -0600

    new changes added

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -41,6 +41,61 @@ describe("First test suite", () => {
     // contains - find HTML text and by text and locator
     cy.contains("Sign in");
     cy.contains('[status="warning"]', "Sign in");
-    cy.contains("nb-card", "Horizontal Form");
+    cy.contains("nb-card", "Horizontal form").find("button");
+    cy.contains("nb-card", "Horizontal form").contains("Sign in");
+    cy.contains("nb-card", "Horizontal form").get("button");
+
+    //cypress chains and DOM
+    cy.get("#inputEmail3")
+      .parents("form")
+      .find("button")
+      .should("contain", "Sign in")
+      .parents("form")
+      .find("nb-checkbox")
+      .click();
+  });
+
+  it("save subject of the command", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[for="inputEmail1"]')
+      .should("contain", "Email");
+    cy.contains("nb-card", "Using the Grid")
+      .find('[for="inputPassword2"]')
+      .should("contain", "Password");
+
+    //Not correct on Cypress
+    //const usingTheGrid = cy.contains("nb-card", "Using the Grid");
+    //usingTheGrid.find('[for="inputPassword2"]').should("contain", "Password");
+
+    //1 Cypress Alias
+    cy.contains("nb-card", "Using the Grid").as("usingTheGrid");
+    cy.get("@usingTheGrid")
+      .find('[for="inputEmail1"]')
+      .should("contain", "Email");
+    cy.get("@usingTheGrid")
+      .find('[for="inputPassword2"]')
+      .should("contain", "Password");
+
+    // 2 Cypress then() methods
+    cy.contains("nb-card", "Using the Grid").then((usingTheGridFrom) => {
+      cy.wrap(usingTheGridFrom)
+        .find('[for="inputEmail1"]')
+        .should("contain", "Email");
+      cy.wrap(usingTheGridFrom)
+        .find('[for="inputPassword2"]')
+        .should("contain", "Password");
+    });
+  });
+
+  it.only("extract text values", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+    //1
+    cy.get('[for="exampleInputEmail1"]').should("contain", "Email address");
   });
 });
